5c83c506c09431f103b28debaffa847bbcf7f5ba
TheSkykiller1 <acqua.rium@hotmail.fr>
2018-06-11 21:32:03 +0200

    Fixed some things and R1

--- app/src/main/java/borioito/gamesup/event.java
+++ app/src/main/java/borioito/gamesup/event.java
@@ -1,5 +1,6 @@
 package borioito.gamesup;
 
+import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
@@ -10,6 +11,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.*;
@@ -17,47 +19,161 @@ import android.view.*;
 import java.util.ArrayList;
 import java.util.List;
 import android.widget.AdapterView.OnItemClickListener;
-
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class event extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private ListView listviewGames;
-    private List<Event_list_games> games_List_items;
-    private Event_list_adapter adapter;
+    ListView listviewGames;
+    List<Event_list_games> games_List_items;
+    Event_list_adapter adapter;
+    int notification_send=0;//On n'envoi que une fois la notif tant que l'application est ouverte
+    String filter="None";//Les filtres
+    String global_event="Yes";//Yes = tous les event, No = Follow only
+
+    public class Event_list_adapter extends BaseAdapter {
+
+        private Context mContext;
+
+        Event_list_adapter(Context context) {
+            this.mContext = context;
+        }
+        @Override
+        public int getCount() {
+            return games_List_items.size();
+        }
+        @Override
+        public Object getItem(int position) {
+            return games_List_items.get(position);
+        }
+        @Override
+        public long getItemId(int position) {
+            return games_List_items.indexOf(getItem(position));
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = View.inflate(mContext,R.layout.list_gamesevent, null);
+
+            //On appelle tous les parametres a remplir
+            TextView texttitre = (TextView)v.findViewById(R.id.text_titre);
+            TextView textplateforme = (TextView)v.findViewById(R.id.text_plateforme);
+            TextView textdate = (TextView)v.findViewById(R.id.text_date);
+            Button textbut_follow =(Button)v.findViewById(R.id.text_but_follow);
+
+            //On rempli les parametre
+            texttitre.setText(games_List_items.get(position).getTitre());
+            textplateforme.setText(games_List_items.get(position).getPlateforme());
+            textdate.setText(games_List_items.get(position).getDate());
+
+            //On creer les actions pour les boutons
+            final int position_object = position;
+            final View viewer=v;
+            if(games_List_items.get(position).getIs_follow()==1){
+                textbut_follow.setText("Unfollow");
+                /**On choisi l'action du bouton unfollow*/
+                textbut_follow.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Button but_follow_state =(Button)viewer.findViewById(R.id.text_but_follow);
+                        but_follow_state.setText("Follow");
+                        games_List_items.get(position_object).setIs_follow(0);
+                        Log.i("Suivi","Event non suivi "+position_object+" Et donnée "+games_List_items.get(position_object).getIs_follow());
+                    }
+                });
+            }
+            else{
+                textbut_follow.setText("Follow");
+                /**On choisi l'action du bouton follow*/
+                textbut_follow.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Button but_follow_state =(Button)viewer.findViewById(R.id.text_but_follow);
+                        but_follow_state.setText("Unfollow");
+                        games_List_items.get(position_object).setIs_follow(1);
+                        Log.i("Suivi","Event suivi "+position_object+" Et donnée "+games_List_items.get(position_object).getIs_follow());
+                    }
+                });
+            }
+            v.setTag(games_List_items.get(position).getId_component());
+            return v;
+        }
+
+    }
+
+    public void get_event_from_database()
+    {
+        //Take all data after a certain date-1
+        //SELECT idevent,titre,date FROM table WHERE TheNameOfTimestampColumn > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 1 DAY)
+        /*
+        a terminer (notif)
+        SELECT  idevent,titre,date
+        FROM    event
+        WHERE   date >= DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 1 DAY) AND
+        date   <= DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 1 DAY)
+        */
+        ////
+        //Todo appliquer les filtres
+        //Todo choisir le mode de visualition
+        //Todo connection bdd
+        //Todo Notif grace a la bdd directement
+
+        games_List_items = new ArrayList<>();
+        games_List_items.add(new Event_list_games(1,1,"CSGO","XBOX","23/05/2018",0));
+        games_List_items.add(new Event_list_games(2,15,"battlefield","PC","23/05/2018",0));
+        games_List_items.add(new Event_list_games(3,85,"overwatch","PC","23/05/2018",1));
+        games_List_items.add(new Event_list_games(4,56,"Little pony","PS4","23/05/2018",1));
+        games_List_items.add(new Event_list_games(5,441,"Mario","wii","23/05/2018",0));
+        games_List_items.add(new Event_list_games(6,57,"Fortnite","PC","23/05/2018",1));
+        games_List_items.add(new Event_list_games(7,15,"didisco","XBOX","23/05/2018",0));
+        games_List_items.add(new Event_list_games(8,55,"Looss","PS4","23/05/2018",0));
+
+
+        /*int notification_send=0;
+        String filter="None";//Les filtres
+        String global_event="Yes";//Yes = tous les event, No = Follow only
+        for (Event_list_games item : games_List_items) {
+            if (item.getIs_follow()==1) {
+                Log.i("Suivi","Article suivi"+item.getId_article());
+                if(notification_send==0){
+
+                }
+            }
+        }*/
+
+    }
+    public void actualiser_listview(){
+        listviewGames = (ListView)findViewById(R.id.list);
+        get_event_from_database();
+
+        //Ajoute les données a la listview
+        adapter = new Event_list_adapter(getApplicationContext());
+        listviewGames.setAdapter(adapter);
+        //listviewGames.setItemsCanFocus(false);
+        /**Si l'on clique sur le cadre du jeu on lance un évenement qui affiche un toast*/
+        listviewGames.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id){
+                Toast.makeText(getApplicationContext(), "Click on=" + view.getTag()+" Position: "+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
 
-
         /**ListView des releases*/
-
-        listviewGames = (ListView)findViewById(R.id.list);
-        games_List_items = new ArrayList<>();
-
-        games_List_items.add(new Event_list_games(1,"CSGO","XBOX","23/05/2018"));
-        games_List_items.add(new Event_list_games(2,"battlefield","PC","23/05/2018"));
-        games_List_items.add(new Event_list_games(3,"overwatch","PC","23/05/2018"));
-        games_List_items.add(new Event_list_games(4,"Little pony","PS4","23/05/2018"));
-        games_List_items.add(new Event_list_games(5,"Mario","wii","23/05/2018"));
-        games_List_items.add(new Event_list_games(6,"Fortnite","PC","23/05/2018"));
-        games_List_items.add(new Event_list_games(7,"didisco","XBOX","23/05/2018"));
-        games_List_items.add(new Event_list_games(8,"Looss","PS4","23/05/2018"));
-
-        adapter = new Event_list_adapter(getApplicationContext(),games_List_items);
-        listviewGames.setAdapter(adapter);
-        listviewGames.setItemsCanFocus(false);
-        listviewGames.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick (AdapterView<?> parent, View view, int position, long id){
-                Toast.makeText(getApplicationContext(), "Click on=" + view.getTag(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        actualiser_listview();
 
         /**Menu Navigation!*/
+        navigation_init();
+    }
+    public void navigation_init()
+    {
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -70,7 +186,6 @@ public class event extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
-
     /**Menu Navigation! Commande d'interface*/
     @Override
     public void onBackPressed() {
--- backuppp/r1/Event_list_games.java
+++ backuppp/r1/Event_list_games.java
@@ -8,49 +8,46 @@ public class Event_list_games {
 	private String titre;
 	private String plateforme;
 	private String date;
-	private int id_component;
-	private int id_article;
-	private int is_follow;
+	private int id;
 
-	public Event_list_games(int id_component, int id_article, String titre/*,int pics_id*/,String plateforme, String date, int is_follow) {
+	public Event_list_games(int id, String titre/*,int pics_id*/,String plateforme, String date) {
         //this.pics_id = pics_id;
-        this.id_component = id_component;
-        this.id_article = id_article;
+        this.id = id;
 	    this.titre = titre;
 		this.plateforme = plateforme;
 		this.date = date;
-		this.is_follow= is_follow;
 	}
 	//Setter, getter
 
-    public int getIs_follow() {
-        return is_follow;
+    public int getId() {
+        return id;
     }
 
-    public void setIs_follow(int is_follow) {
-        this.is_follow = is_follow;
+    public void setId(int id) {
+        this.id = id;
     }
-
-    public int getId_component() {
-        return id_component;
-    }
-
-    public int getId_article() {
-        return id_article;
-    }
-
 
     public String getTitre() {
         return titre;
     }
 
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
 
     public String getPlateforme() {
         return plateforme;
+    }
+
+    public void setPlateforme(String plateforme) {
+        this.plateforme = plateforme;
     }
 
     public String getDate() {
         return date;
     }
 
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
